cca80e1b78a0daab09f1c5402399bc145bffba5c
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-03-12 18:54:33 -0300

    AJAX com Jquery

--- Ajax/main.js
+++ Ajax/main.js
@@ -1,12 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("btn-buscar-cep").addEventListener("click", function() {
-        // AJAX - Assincronous javascript and xml request
-        const xhtpp = new XMLHttpRequest();
-        const cep = document.getElementById("cep").value;
+// document.addEventListener("DOMContentLoaded", function(){
+//     document.getElementById("btn-buscar-cep").addEventListener("click", function() {
+//         // AJAX - Assincronous javascript and xml request
+//         const xhtpp = new XMLHttpRequest();
+//         const cep = document.getElementById("cep").value;
+//         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+//         xhtpp.open('GET', endpoint);
+//         xhtpp.send()
+        
+//     })
+// })
+
+$(document).ready(function() {
+    $('#btn-buscar-cep').click(function() {
+        const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
 
-        xhtpp.open('GET', endpoint);
-        xhtpp.send()
-        
+        $.ajax(endpoint).done(function(res) {
+            const logradouro = res.logradouro;
+            const bairro = res.bairro;
+            const cidade = res.localidade;
+            const estado = res.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
+        })
     })
 })
